613b8f94b975a6cd07c40b3fac37d253df1db1f0
robpocko <robpocko@users.noreply.github.com>
2019-06-18 11:03:33 +1000

    New BF class

---
+++ JobLogger.BF/Tools/ToolsBF.cs
@@ -0,0 +1,24 @@
+﻿using JobLogger.BF.ListModels;
+using JobLogger.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JobLogger.BF.Tools
+{
+    public class ToolsBF
+    {
+        private JobLoggerDbContext db;
+
+        public ToolsBF(JobLoggerDbContext db)
+        {
+            this.db = db;
+        }
+
+        public bool BackupDatabase()
+        { 
+            return true;
+        }
+    }
+}
