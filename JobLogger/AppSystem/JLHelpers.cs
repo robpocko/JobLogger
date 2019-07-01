773584b0bbd0feec113145eb8691e4759462eaab
robpocko <robpocko@users.noreply.github.com>
2019-07-01 16:35:11 +1000

    Stuff

--- JobLogger/AppSystem/JLHelpers.cs
+++ JobLogger/AppSystem/JLHelpers.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JobLogger.AppSystem.DataAccess;
+using System;
+using System.Collections.Generic;
 
 namespace JobLogger.AppSystem
 {
@@ -6,5 +8,6 @@ namespace JobLogger.AppSystem
     {
         public static TimeSpan TimeFromDateTime(DateTime date) => date.TimeOfDay;
         public static string ShowCommentToolTip(int nbr, string entity) => string.Format("There are {0} comments for this {1}", nbr, entity);
+        internal static bool EnableShowCommentsButton<T>(List<T> comments) where T : CommentAPI => comments != null && comments.Count > 0;
     }
 }
